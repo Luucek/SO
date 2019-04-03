8287dc46bfcf8d1a2301e8473650ef624a5250bf
Luucek <wszolapatryk@gmail.com>
2019-04-03 16:05:26 +0200

    Add lab03 final file

--- Lab03/41563.so2.lab03.main.c
+++ Lab03/41563.so2.lab03.main.c
@@ -1,3 +1,9 @@
+//SO2 IS1 210B LAB03
+//Patryk Wszola
+//wp41563@zut.edu.pl
+//gcc 41563.so2.lab03.main.c -o lab03
+//./lab03 abcde
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -9,6 +15,7 @@ int isPower(int len);
 
 int main(int argc, char *argv[])
 {
+    int status = 0;
     if (argv[1] == NULL)
     {
         printf("Brak argumentu!\n");
@@ -31,18 +38,23 @@ int main(int argc, char *argv[])
             {
                 arg[i] = argv[1][i];
             }
-            arg[newLength + 1] = '\0';
+            arg[newLength] = '\0';
         }
         if (!arg)
             arg = argv[1];
 
-        int *status;
-
         char *left;
         char *right;
+        char *sen;
+        //alokacja pamieci na cala sciezke
+        if (!argv[2])
+            sen = malloc((strlen(arg) * strlen(arg)) * sizeof(char));
+        else
+            sen = argv[2];
 
         if (strlen(arg) > 1)
         {
+            //alokacja pamieci na kolejne watki
             int mid = strlen(arg) / 2;
             left = malloc((mid + 1) * sizeof(char));
             right = malloc((mid + 1) * sizeof(char));
@@ -54,34 +66,43 @@ int main(int argc, char *argv[])
             }
             left[mid] = '\0';
             right[mid] = '\0';
+
+            strcat(sen, arg);
+            strcat(sen, " ");
+
+            pid_t pid = fork();
+
+            if (pid == 0)
+                execl("./lab03", "./lab03", left, sen, (char *)NULL);
+
+            pid_t pid2 = fork();
+
+            if (pid2 == 0)
+                execl("./lab03", "./lab03", right, sen, (char *)NULL);
         }
         else
         {
-            printf("%s ", arg);
+            strcat(sen, arg);
+            printf("%d %s\n", getpid(), sen);
             exit(1);
         }
 
-        //printf("Ciag znakow: %s\n", arg); // do usuniecia
+        if (wait(&status))
+        {
+            if (!WIFEXITED(status))
+                printf("BLAD wait()");
+            else
+                ;
+        }
+        if (wait(&status))
+        {
+            if (!WIFEXITED(status))
+                printf("BLAD wait()");
+            else
+                ;
+        }
 
-        pid_t pid = fork();
-
-        if (pid == 0)
-        {
-            // child process
-            execl("./lab03", "./lab03", left, (char *)NULL);
-            execl("./lab03", "./lab03", right, (char *)NULL);
-        }
-        else if (pid > 0)
-        {
-            // parent process
-            waitpid(pid, status, 0);
-            //printf("%s ", arg);
-        }
-        else
-        {
-            // fork failed
-            return -2;
-        }
+        printf("%d %s\n", getpid(), sen);
     }
 
     return 1;
