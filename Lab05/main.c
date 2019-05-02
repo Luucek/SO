9557145c1bc23acbc0944fca3f724d9aac55ebf9
Luucek <wszolapatryk@gmail.com>
2019-05-02 22:01:05 +0200

    Add options to lab05

--- Lab05/main.c
+++ Lab05/main.c
@@ -6,18 +6,20 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 
 int filesCounter = 0, foldersCounter = 0;
+int flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0;
 
 void listDirectory(char *arg, int indent)
 {
     struct dirent **namelist;
     int n = 0; //ilosc wszystkich plikow
     char **folders, **foldersNames;
-    folders = malloc(30 * sizeof(int *));
+    folders = malloc(30 * sizeof(char *));
     for (int i = 0; i < 30; i++)
         folders[i] = malloc(FILENAME_MAX);
-    foldersNames = malloc(30 * sizeof(int *));
+    foldersNames = malloc(30 * sizeof(char *));
     for (int i = 0; i < 30; i++)
         foldersNames[i] = malloc(FILENAME_MAX);
     int f = 0; //ilosc folderow
@@ -40,11 +42,18 @@ void listDirectory(char *arg, int indent)
             {
                 if (namelist[n]->d_name[0] != '.')
                 {
-                    for (int i = 0; i < indent; i++)
-                        printf("   ");
 
-                    printf("|> %s\n", namelist[n]->d_name);
-                    filesCounter++;
+                    if (flag1 == 0)
+                    {
+                        for (int i = 0; i < indent; i++)
+                            printf("   ");
+                        if (flag2 == 0)
+                            printf("|> %s\n", namelist[n]->d_name);
+                        else
+                            printf("|> %s\n", path);
+
+                        filesCounter++;
+                    }
                 }
                 free(namelist[n]);
             }
@@ -65,11 +74,17 @@ void listDirectory(char *arg, int indent)
         {
             for (int i = 0; i < indent; i++)
                 printf("   ");
+            if (flag2 == 0)
+                printf("|# %s\n", foldersNames[f]);
+            else
+                printf("|# %s\n", folders[f]);
 
-            printf("|# %s\n", foldersNames[f]);
             strcat(folders[f], "/");
-            listDirectory(folders[f], ++indent);
-            indent--;
+            if (indent + 1 < flag4 || flag4 == 0)
+            {
+                listDirectory(folders[f], ++indent);
+                indent--;
+            }
             free(folders[f]);
         }
         free(folders);
@@ -78,10 +93,47 @@ void listDirectory(char *arg, int indent)
 
 int main(int argc, char *artv[])
 {
-    if (!artv[1])
+    int opt;
+    while ((opt = getopt(argc, artv, "dflL:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'd':
+            flag1++;
+            break;
+        case 'f':
+            flag2++;
+            break;
+        case 'l':
+            flag3++;
+            break;
+        case 'L':
+            flag4 = atoi(optarg);
+            if (flag4 < 0 || flag4 > 10)
+            {
+                printf("BLAD: argument tylko z zakresu od 0 do 10\n");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        default:
+            printf("Uzycie: ./program [-dfl] [-L poziom] [sciezka]\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    if (argc == 1)
         listDirectory("./", 0);
     else
-        listDirectory(artv[1], 0);
+    {
+        if (artv[argc - 1][0] != '-')
+            listDirectory(artv[argc - 1], 0);
+        else
+            listDirectory("./", 0);
+    }
 
-    printf("\n%d folderow, %d plikow\n", foldersCounter, filesCounter);
+    if (foldersCounter > 0 && filesCounter > 0)
+        printf("\n%d folderow, %d plikow\n", foldersCounter, filesCounter);
+    else if (foldersCounter > 0 && filesCounter == 0)
+        printf("\n%d folderow\n", foldersCounter);
+    else
+        printf("\n%d plikow\n", filesCounter);
 }
